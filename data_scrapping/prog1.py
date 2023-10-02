77d83361694f5ad52e73158a3fda81a8781abe6d
Shreevathsatantry <shreevathstantry@gmail.com>
2023-10-02 13:46:24 +0530

    git commit

--- data_scrapping/prog1.py
+++ data_scrapping/prog1.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-url="https://www.flipkart.com/search?q=chairs&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
-HEADERS=({"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36','Accept-Language':'en-US,en q=0.5'})
+url="https://www.flipkart.com/cameras/dslr-mirrorless/pr?sid=jek%2Cp31%2Ctrv&p%5B%5D=facets.fulfilled_by%255B%255D%3DFlipkart%2BAssured&p%5B%5D=facets.type%255B%255D%3DMirrorless&param=179&ctx=eyJjYXJkQ29udGV4dCI6eyJhdHRyaWJ1dGVzIjp7InZhbHVlQ2FsbG91dCI6eyJtdWx0aVZhbHVlZEF0dHJpYnV0ZSI6eyJrZXkiOiJ2YWx1ZUNhbGxvdXQiLCJpbmZlcmVuY2VUeXBlIjoiVkFMVUVfQ0FMTE9VVCIsInZhbHVlcyI6WyJTaG9wIE5vdyEiXSwidmFsdWVUeXBlIjoiTVVMVElfVkFMVUVEIn19LCJ0aXRsZSI6eyJtdWx0aVZhbHVlZEF0dHJpYnV0ZSI6eyJrZXkiOiJ0aXRsZSIsImluZmVyZW5jZVR5cGUiOiJUSVRMRSIsInZhbHVlcyI6WyJUb3AgTWlycm9ybGVzcyBDYW1lcmFzIl0sInZhbHVlVHlwZSI6Ik1VTFRJX1ZBTFVFRCJ9fSwiaGVyb1BpZCI6eyJzaW5nbGVWYWx1ZUF0dHJpYnV0ZSI6eyJrZXkiOiJoZXJvUGlkIiwiaW5mZXJlbmNlVHlwZSI6IlBJRCIsInZhbHVlIjoiRExMRzJYRENGQlhWVVpUSCIsInZhbHVlVHlwZSI6IlNJTkdMRV9WQUxVRUQifX19fX0%3D"
+HEADERS=({"User-Agent":'','Accept-Language':'en-US,en q=0.5'})
 website=requests.get(url,headers=HEADERS)
 print(website)
 r=requests.get(url)
@@ -27,7 +27,7 @@ Soup=BeautifulSoup(htmlcontent,'html.parser')
     # print(price)
     # review=(Soup.find('div',class_='_3LWZlK').get_text())
     # print(review)
-name= (Soup.find('a',class_='s1Q9rs').get_text())
+name= Soup.find('img',class_='_396cs4')
 price=(Soup.find('div',class_='_30jeq3').get_text())
 rating=Soup.find('div',class_='_3LWZlK _1D-8OL')
     # product.append(name.text)
